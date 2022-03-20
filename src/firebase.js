d93249b3fa98fad46dbc3ebce7652d763dee1a4f
shin <kahkii@naver.com>
2022-03-20 15:30:49 +0900

    환경 변수 설정

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2xim03q9NM2hoFmukchPAjMMMz0IL_nA",
-    authDomain: "twitter-clone-coding-8e2b9.firebaseapp.com",
-    projectId: "twitter-clone-coding-8e2b9",
-    storageBucket: "twitter-clone-coding-8e2b9.appspot.com",
-    messagingSenderId: "77140013167",
-    appId: "1:77140013167:web:338258fcd6b9c50e01dd9a"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
 export default initializeApp(firebaseConfig);
