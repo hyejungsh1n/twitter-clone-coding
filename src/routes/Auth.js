f2c0c9dbc8825130be0d97929ae4edee98d05c66
shin <kahkii@naver.com>
2022-03-24 22:12:51 +0900

    폼, 버튼 생성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,14 @@
 import React from "react";
 
-const Auth = ()=> <span >Auth</span>;
+const Auth = ()=> <div >
+    <form>
+        <input type="text" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <input type="submit" value="Login" />
+    </form>
+    <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+    </div>
+    </div>;
 export default Auth;
