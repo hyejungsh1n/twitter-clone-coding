b10caeb7dbd29039955ec21e109505c388ebab2e
shin <kahkii@naver.com>
2022-03-24 22:25:52 +0900

    상태 관리

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React from "react"; 
 
-const Auth = ()=> <div >
-    <form>
-        <input type="text" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        <input type="submit" value="Login" />
-    </form>
-    <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
-    </div>
-    </div>;
+const Auth = () => { 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        console.log(event.target.name)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    return ( 
+        <div >
+        <form onSubmit={onSubmit}>
+            <input 
+                name="email" 
+                type="text" 
+                placeholder="Email" 
+                required 
+                value={email}
+                onChange={onChange}  />
+            <input 
+                name="password" 
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChange} />
+            <input
+                type="submit" 
+                value="Login" />
+        </form>
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+        </div>
+        </div>
+    );
+};
 export default Auth;
