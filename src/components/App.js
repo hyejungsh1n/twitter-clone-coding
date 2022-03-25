08193851a3c9abefb1e497d6b54660b4f5b41577
shin <kahkii@naver.com>
2022-03-25 23:10:00 +0900

    로그인 상태 관찰하는 메소드 추가

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); 
 
   useEffect(() => {
-
+    // onAuthStateChanged 사용자의 로그인 상태의 변화를 관찰하는 관찰자역할 추가
+    authService.onAuthStateChanged((user) => console.log(user))
   }, [])
   return (
     <>
