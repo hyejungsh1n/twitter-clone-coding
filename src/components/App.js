ae43cdd89c58971a764ccfd30528fb30621bffb5
shin <kahkii@naver.com>
2022-03-26 16:58:46 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,18 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false); // 초기화
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); 
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
   useEffect(() => {
     // onAuthStateChanged 사용자의 로그인 상태의 변화를 관찰하는 관찰자역할 추가
-    authService.onAuthStateChanged((user) => console.log(user))
+    authService.onAuthStateChanged((user) => {
+    if(user) {
+      setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true); // init이 false라면 router를 숨김
+    });
   }, [])
   return (
     <>
