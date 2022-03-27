75c43c0b870b64a5bd6a909846bfd6a79ed9a8dd
shin <kahkii@naver.com>
2022-03-27 22:56:02 +0900

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 const Navigation = () => 
     <nav>
         <ul>
-            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">Home!</Link></li>
         </ul>
         <ul>
             <li><Link to="/profile">Profile</Link></li>
