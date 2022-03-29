c5e89cd7f682b35980fe7b29102b8f737ebd2176
shin <kahkii@naver.com>
2022-03-29 23:13:15 +0900

    UPDATE

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function App() {
   const [init, setInit] = useState(false); // 초기화
   const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
+  // 복습
   useEffect(() => {
     // onAuthStateChanged 사용자의 로그인 상태의 변화를 관찰하는 관찰자역할 추가
     authService.onAuthStateChanged((user) => {
