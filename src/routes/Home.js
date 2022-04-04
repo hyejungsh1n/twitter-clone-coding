5d55186654d3b392448fb6240be46f43c9f1be11
shin <kahkii@naver.com>
2022-04-04 19:16:33 +0900

    form 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span >Home</span>;
+const Home = () => {
+        const [tweet, setTweet] = useState(""); 
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const { 
+            target: {value},
+    } = event; // event안 target의 value를 달라고 하는 것?
+    setTweet(value);
+    }
+return (
+<div>
+    <form>
+        <input type={tweet} onChange={onChange}  placeholder="What's on your mind" maxLength={120} />
+        <input type="submit" value="Tweet" />
+    </form>
+</div>
+    );
+};
 export default Home;
